a0d246227987aa3bf43b8708529a1deb6407ab5a
leonilsoneduardo <Leonilson240@gmail.com>
2016-07-22 11:23:13 -0300

    Update adm.sql
    adicionei mais coisas no nosso Banco

--- _bancoDeDados/adm.sql
+++ _bancoDeDados/adm.sql
@@ -2,14 +2,70 @@ CREATE DATABASE atualizar DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_gerera
 
 USE atualizar;
 		
+/*tabelas da pagina home*/
+		
+		CREATE TABLE HistoriaDaIgreja (
+				tipo VARCHAR (18), /* aqui vai ficar o HistoriaDaIgreja*/
+				texto TEXT,
+		)DEFAULT CHARSET = utf8;
+		
+		
+		
+		
+		
 		CREATE TABLE administrador (
-				LONGIN VARCHAR(30) NOT NULL,
-				SENHA VARCHAR(30) NOT NULL
+				  LONGIN VARCHAR(30) NOT NULL,
+				  SENHA VARCHAR(30) NOT NULL
 		)DEFAULT CHARSET = utf8;
 
 
 
 
+					
+					CREATE TABLE pedidosEaconselhamento (
+						        nome VARCHAR(30),
+							email VARCHAR(40),
+							texto TEXT
+					)DEFAULT CHARSET = utf8;
+					
+					
+					
+					
+					/*tabela da pagina igrejas*/
+									
+									CREATE TABLE nossasLocalizaçoes (
+											   id INT NOT NULL AUTO_INCREMENT,
+											   tipo VARCHAR (9), /* aqui vai ficar as áres*/
+											   texto TEXT,
+											   PRIMARY KEY(id)
+									)DEFAULT CHARSET = utf8;
+									
+									
+									
+									
+									CREATE TABLE nossosLideres (
+											id INT NOT NULL AUTO_INCREMENT,
+											tipo VARCHAR (30), /* aqui vai ficar Nossos Líderes*/
+											texto TEXT,
+											PRIMARY KEY(id)
+					                                )DEFAULT CHARSET = utf8;
+									
+					
+					
+						CREATE TABLE agendaDoTemploSede (
+								id INT NOT NULL AUTO_INCREMENT,
+								tipo VARCHAR (9), /* aqui vai ficar Nossos Líderes*/
+								texto TEXT,
+								PRIMARY KEY(id)
+					)DEFAULT CHARSET = utf8;
+					
+					
+					
+					
+					/*Tabela das paginas dos Ministério*/
+					
+					
+					
 					CREATE TABLE ministerios (
 							id INT NOT NULL AUTO_INCREMENT,
 							tipo VARCHAR (9), /* aqui vai ficar o tipo do ministerio  (MASCULINO/FEMININO/CASAIS/JOVEM/INFANTIL)*/
@@ -20,12 +76,3 @@ USE atualizar;
 							foto3 BLOB NOT NULL
 							PRIMARY KEY(id)
 					)DEFAULT CHARSET = utf8;
-
-
-
-									CREATE TABLE pedidosEaconselhamento (
-											nome VARCHAR(30),
-											email VARCHAR(40),
-											texto TEXT
-									)DEFAULT CHARSET = utf8;
-
